a90921e7f0335aa24b46ae251aca9804ddc1e6c2
jedier stivenson correa amaris <153353269+Stivenco12@users.noreply.github.com>
2025-04-02 16:52:10 -0500

    feat: ✨ se añadio un archivo de inserciones sql

---
+++ insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO user (name, email, password, type) VALUES
+('admin', 'admin@company.com', 'admin', 'admin'),
+('User1', 'user1@company.com', 'User1', 'cliente'),
+('User2', 'user2@company.com', 'User2', 'cliente');
